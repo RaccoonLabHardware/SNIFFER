1b62b644ff63e9e72783156d411d6e92787fed9f
sainquake <sainquake@gmail.com>
2023-11-08 10:06:35 +0300

    Pr black magic (#3)
    * routed
    
    * outputs and precommit
    
    * Delete output.xlsx
    
    * updated
    
    * update pics
    
    * update with 2 mosfets
    
    * update with runes
    
    * update

--- precommit.py
+++ precommit.py
@@ -350,7 +350,7 @@ f.write('|  | <img src="doc/r-view-top.jpg" alt="drawing" width="300"/> | <img s
 f.write('\n')
 f.write('## Features\n\n')
 
-f.write('# Wiring\n\n')
+f.write('## Wiring\n\n')
 
 f.write('Schematic features. Schematic can be provided via issue.\n\n')
 
@@ -547,7 +547,7 @@ Take a look at the picure in attachements should be installed from the side.
 
 The material provided in this Github repository is subject to the following conditions. 
 
-Firmware files: All firmwares are free (but not open source). Besides unlimited private use you are also granted the permission to use them for commercial purposes under the condition that (1) you don’t modify the firmware, e.g. remove or change copyright statements, (2) provide it for free, i.e. don’t charge any explicit or implicit fees to your customers, and (3) correctly and clearly cite the origin of the firmware and the project web page in any product documentation or web page. 
+Firmware files: All firmwares are free (but not open source). Besides unlimited private use you are also granted the permission to use them for commercial purposes under the condition that (1) you dont modify the firmware, e.g. remove or change copyright statements, (2) provide it for free, i.e. dont charge any explicit or implicit fees to your customers, and (3) correctly and clearly cite the origin of the firmware and the project web page in any product documentation or web page. 
 
 Hardware files: All hardware, for which material is provided, is open source hardware, under the terms of the TAPR Open Hardware License as published by the Free Hardware Foundation, see http://www.tapr.org/ohl.html. The TAPR license explicitly permits essentially unlimited commercial use, with only few conditions such as that copyright logos are not removed.
 
